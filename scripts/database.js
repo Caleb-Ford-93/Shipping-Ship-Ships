99e59e9eb63eb546673bce552c65f4c31afa16d3
Caleb Ford <caleb.ford.sto@gmail.com>
2024-06-10 10:59:57 -0500

    added shipping ship feature

--- scripts/database.js
+++ scripts/database.js
@@ -6,18 +6,35 @@ const database = {
         { id: 4, location: "Antwerp, Belgium", volume: "12.04" }
     ],
     haulers: [
-        { id: 1, name: "Haulistic Solution", dockId: 1},
+        { id: 1, name: "Haulistic Solutions", dockId: 1},
         { id: 2, name: "Haul Me To The Moon", dockId: 2},
         { id: 3, name: "Haul-o", dockId: 3},
         { id: 4, name: "It's Haul Over", dockId: 1},
-        { id: 5, name: "Big Hauler", dockId: 4},
+        { id: 5, name: "I'm Haul Over It", dockId: 4},
     ],
+    shippingShips: [
+        { id: 1, name: "I Ship That", haulerID: 1},
+        { id: 2, name: "Oh Ship", haulerID: 1},
+        { id: 3, name: "Just A Ship", haulerID: 1},
+        { id: 4, name: "Joined At The Ship", haulerID: 1},
+        { id: 5, name: "Stiff Upper Ship", haulerID: 1},
+        { id: 6, name: "This Ship Has Sailed", haulerID: 1},
+        { id: 7, name: "I Ship You Not", haulerID: 1},
+        { id: 8, name: "Water-Ship Down", haulerID: 1},
+        { id: 9, name: "Sea-Ship Enterprise", haulerID: 1},
+        { id: 10, name: "If It Fits It Ships", haulerID: 1},
+        { id: 11, name: "Jump Hop And a Ship", haulerID: 1},
+    ]
 }
 
 export const getDocks = () => {
-    database.docks.map((dock) => ({...dock}))
+    database.docks.map((dock) => ({ ...dock }))
 }
 
 export const getHaulers = () => {
-    database.haulers.map((hauler) => ({...hauler}))
+    database.haulers.map((hauler) => ({ ...hauler }))
+}
+
+export const getShippingShips = () => {
+    database.shippingShips.map((ship) => ({ ...ship }))
 }
