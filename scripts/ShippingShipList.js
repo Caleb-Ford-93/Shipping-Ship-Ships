921223b9c9cfe116d44792b5d1af578537b1d21e
Caleb Ford <caleb.ford.sto@gmail.com>
2024-06-10 13:24:53 -0500

    added shipping ships click event

--- scripts/ShippingShipList.js
+++ scripts/ShippingShipList.js
@@ -1,4 +1,4 @@
-import { getShippingShips } from "./database.js"
+import { getHaulers, getShippingShips } from "./database.js"
 
 export const ShippingShipList = () => {
     const shippingShips = getShippingShips()
@@ -6,10 +6,34 @@ export const ShippingShipList = () => {
     let shippingShipHTML = "<ul>"
 
     for (const shippingShip of shippingShips) {
-        shippingShipHTML += `<li>${shippingShip.name}</li>`
+        shippingShipHTML += `<li data-type="ship" 
+                                 data-name="${shippingShip.name}"
+                                 data-id="${shippingShip.id}" 
+                                 data-haulerId="${shippingShip.haulerId}">
+                              ${shippingShip.name}</li>`
     }
 
     shippingShipHTML += "</ul>"
 
     return shippingShipHTML
 }
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+
+        if(itemClicked.dataset.type === "ship"){
+            const haulers = getHaulers()
+            const shipHaulerId = parseInt(itemClicked.dataset.haulerid)
+            let haulingShip = {}
+            for (const hauler of haulers) {
+                if(hauler.id === shipHaulerId){
+                    haulingShip = hauler
+                }
+                
+            }
+            window.alert(`${itemClicked.dataset.name} is being hauled by ${haulingShip.name}`)
+        }
+    }
+)
